ee3f01a42d2f181815ffadc24616dc84f0a864d7
narendrakumar39062-afk <narendrakumar39062@gmail.com>
2026-05-01 15:58:02 +0530

    Create main.jsx

---
+++ main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Portfolio from './index.jsx' // This links to your code
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Portfolio />
+  </React.StrictMode>
+)
